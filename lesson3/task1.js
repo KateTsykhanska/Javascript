1dddda5fc313a07f7338a19a382eb595ac41b492
Katia <kateeegold+1@gmail.com>
2025-03-18 19:17:58 +0100

    commit

---
+++ lesson3/task1.js
@@ -0,0 +1,10 @@
+const a = 5;
+const b = 15;
+const c = 20; 
+
+console.log(a + b);
+console.log(b - c);
+console.log (a * c);
+console.log (b / a);
+console.log (c % b);
+console.log(Math.pow(a, b));
