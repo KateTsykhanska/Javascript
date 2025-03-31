2a33a1dee2b7b9e05a77bcb50d77c7f2e41a085c
Katia <kateeegold+1@gmail.com>
2025-03-31 21:31:48 +0200

    formatting

--- lesson6/task1.js
+++ lesson6/task1.js
@@ -1,15 +1,15 @@
 function handleNum(num, handleEven, handleOdd) {
-if (num % 2 === 0) {
-    handleEven(num)
-} else {
-handleOdd(num)
+    if (num % 2 === 0) {
+        handleEven(num)
+    } else {
+        handleOdd(num)
+    }
 }
-}
-function handleEven(num){
-console.log("Number is even")
+function handleEven(num) {
+    console.log("Number is even")
 }
 function handleOdd(num) {
-console.log("Number is odd")
+    console.log("Number is odd")
 }
 
 handleNum(7, handleEven, handleOdd)
